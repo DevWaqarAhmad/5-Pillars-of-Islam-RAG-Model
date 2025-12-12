4987ec5fb7bf2d39de795286099ddd01f6c44300
Waqar Ahmad <devwaqarahmad@gmail.com>
2025-12-12 18:07:53 +0500

    Embedd Data

--- embeddings.py
+++ embeddings.py
@@ -1,9 +1,38 @@
 import os
 import json
-from dotenv import load_dotenv
-import google.generativeai as genai
-from google.generativeai import embeddings
+from sentence_transformers import SentenceTransformer
 
-# Load API key
-load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def load_files(folder="data"):
+    docs = []
+    for file in os.listdir(folder):
+        if file.endswith(".txt"):
+            path = os.path.join(folder, file)
+            with open(path, "r", encoding="utf-8") as f:
+                docs.append({
+                    "title": file,
+                    "content": f.read()
+                })
+    return docs
+
+def create_embeddings():
+    docs = load_files()
+
+    embedded_data = []
+    for doc in docs:
+        vector = model.encode(doc["content"]).tolist()
+
+        embedded_data.append({
+            "title": doc["title"],
+            "content": doc["content"],
+            "embedding": vector
+        })
+
+    with open("embeddings.json", "w", encoding="utf-8") as f:
+        json.dump(embedded_data, f, indent=2)
+
+    print("Local Embeddings Generated Successfully!")
+
+if __name__ == "__main__":
+    create_embeddings()
